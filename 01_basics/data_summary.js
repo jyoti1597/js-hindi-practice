e2fb15b348d9ad736ba4be0f4037d111f3420662
jyoti1597 <jyotichilwal1@gmail.com>
2026-05-18 16:30:16 +0530

    datatype summary

---
+++ 01_basics/data_summary.js
@@ -0,0 +1,48 @@
+//Primitive
+
+// 7 types of primitive data types in JavaScript
+// 1. String
+// 2. Number
+// 3. Boolean
+// 4. Null
+// 5. Undefined
+// 6. Symbol
+// 7. BigInt
+
+
+const score = 100
+const scoreValue =100.3
+
+const isloggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('12345')
+const anotherId = Symbol('12345')
+
+console.log(id === anotherId) // Output: false
+
+const bigNumber = 9007199254740991n
+
+
+//what is javascript type  static or dynamic?JavaScript is a dynamic type language, which means that the type of a variable can change at runtime. In the example above, the variable `score` is assigned a boolean value (`false`), but it can be reassigned to a different type (e.g., a number, string, etc.) later in the code without causing an error.
+
+//Reference(Non-primitive)
+
+//Array, Object, Function
+
+
+const heros = ['Batman', 'Superman', 'Wonder Woman'];
+
+const myObj = { name: 'John', age: 30, city: 'New York' };
+
+const myFunc = function() {
+    console.log('Hello, World!')
+}   
+
+// find the type of data
+
+console.log(typeof score) // Output: number
+
+
+// in return all they give object as a type of data
