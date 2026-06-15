3ff4c7c400de24be3aafe243a5d2d68be5292030
jyoti1597 <jyotichilwal1@gmail.com>
2026-06-15 20:57:52 +0530

    order array loops

--- 05_iterations/three.js
+++ 05_iterations/three.js
@@ -2,7 +2,8 @@
 
 // ["", "", ""]
 // [{}, {}, {}]
-
+// iterator is object, array and string
+// it which things we want to use loop
 const arr = [1, 2, 3, 4, 5]
 
 for (const num of arr) {
@@ -15,6 +16,9 @@ for (const greet of greetings) {
 }
 
 // Maps
+//this object itself. the map object hold key value pairs and remembers the original insertion order of the keys
+
+
 
 const map = new Map()
 map.set('IN', "India")
@@ -32,7 +36,7 @@ for (const [key, value] of map) {
 const myObject = {
     game1: 'NFS',
     game2: 'Spiderman'
-}
+}// this is not iterable
 
 // for (const [key, value] of myObject) {
 //     console.log(key, ':-', value);
